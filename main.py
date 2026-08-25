8c811f9c2ef413deb5a5f3818f88af64466fe3a1
minwoo-3 <s26002@gsm.hs.kr>
2026-08-26 08:09:13 +0900

    init: /login Get 요청과 경로 매핑

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def root_handler():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/login")
+def root_handler():
+    return {"message": "로그인 페이지에 오신 것을 환영합니다"}
